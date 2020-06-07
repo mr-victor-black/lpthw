93a5c90bdf13ad059f01de8d477b8c3484371fb3
mr-victor-black <39173134+mr-victor-black@users.noreply.github.com>
2020-06-06 17:21:14 -0700

    Completing Ex 1-4

---
+++ ex3.py
@@ -0,0 +1,33 @@
+print("I will now count my chickens:")
+
+#Here I'm adding up 25 with 30/6, which should get me 30 since it's 25+5
+print("Hens", 25+30 / 6)
+
+# Lets figure out what that percent thing does
+print(3%3)
+print(1%3)
+print(6%3)
+# So it looks like that's a remainder? Or a modulo. Some shit to come back to. This is why I hate math.
+
+
+#There appears to be no difference in x-y vs x - y in terms of output, but apparently it's bad form not to have a space around the operators 
+print("Roosters", 100-25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3+2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5-7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("is it less or equal", 5 <= -2)
---
+++ ex4.py
@@ -0,0 +1,25 @@
+#Defining the amount of cars
+cars = 100
+#Defining the space in a car
+space_in_a_car = 4.0
+#Defining the amount of drivers
+drivers = 30
+#Defining the number of passengers
+passengers = 90
+#Calculating how many cars are not driven
+cars_not_driven = cars - drivers
+#Calculating how many cars are indeed driven
+cars_driven = drivers
+#Calculating the carpool capacity
+carpool_capacity = cars_driven * space_in_a_car
+#Calculating the average passengers in a car
+average_passengers_per_car = passengers / cars_driven
+
+#Now to output everything
+
+print("There are", cars, "cars available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car.")
