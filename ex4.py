df82be9555169393a2b60e4fe7eb138649cd4580
mr-victor-black <39173134+mr-victor-black@users.noreply.github.com>
2020-06-06 17:22:03 -0700

    Updating Ex4 with a comment

--- ex4.py
+++ ex4.py
@@ -23,3 +23,6 @@ print("There will be", cars_not_driven, "empty cars today.")
 print("We can transport", carpool_capacity, "people today.")
 print("We have", passengers, "to carpool today.")
 print("We need to put about", average_passengers_per_car, "in each car.")
+
+
+#I still don't understand when to have a 4.0 vs a 4, for the space_in_a_car variable. The output's looked the same, unless I missed something
