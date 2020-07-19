41aff002eb8fe158be3d9a3a7008859b5d9e4c7c
mr-victor-black <39173134+mr-victor-black@users.noreply.github.com>
2020-07-18 17:37:06 -0700

    Exercise 12

---
+++ ex12.py
@@ -0,0 +1,13 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+#the space at the end is to put a space between the question and your reply
+
+print(f"So you're {age} old, {height} tall, and {weight} heavy.")
+
+
+#Turns out you can type in anything you want into the inputs
+#I wonder if there's a way you can reject non number inputs
+
+#pydoc can be used to define commands in terminal 
