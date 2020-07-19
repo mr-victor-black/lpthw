7d0a6f089431b7f3abf1f77da4425d63a4aa43ad
mr-victor-black <39173134+mr-victor-black@users.noreply.github.com>
2020-07-18 17:32:57 -0700

    Exercise 11

---
+++ ex11.py
@@ -0,0 +1,13 @@
+
+print("How old are you?", end = ' ')
+age = input()
+print("How tall are you?", end = ' ')
+height = input()
+print ("How much do you weight?", end = ' ')
+weight = input()
+
+#the ' ' at the end is what tells Python to end the line with a space, instead of newlining like normal
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+#Reminder to self - print(f) is what tells Python to format the things inside the string
